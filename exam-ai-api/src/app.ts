dd0a278eb56cf74edfcdba44ec65f4d98978e81b
Ed Angelis <ed4ngelis@gmail.com>
2026-06-22 16:10:36 -0300

    Wait collection load

--- exam-ai-api/src/app.ts
+++ exam-ai-api/src/app.ts
@@ -2,12 +2,17 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import routes from "./routes/index";
+import { ready } from "./models/index";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(async (req, res, next) => {
+  await ready;
+  next();
+});
 app.use(routes);
 
 app.get("/", (req, res) => {
